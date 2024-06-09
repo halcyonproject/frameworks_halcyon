30315fc7434c82cd75bbe0dda46a7fd35c99743d
HeroBuxx <me@herobuxx.me>
2024-06-09 13:02:27 +0000

    halcyon: utils: DevicePropsSpoofing: Update Fingerprints
    Kanged from: https://github.com/Evolution-XYZ/frameworks_base/blob/6bc6aac3746be2c88e3cf461c297e5a73639de45/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- utils/src/halcyon/platform/util/DevicePropsSpoofing.java
+++ utils/src/halcyon/platform/util/DevicePropsSpoofing.java
@@ -140,8 +140,8 @@ public class DevicePropsSpoofing {
         propsToChangePixel8Pro.put("PRODUCT", "husky");
         propsToChangePixel8Pro.put("HARDWARE", "husky");
         propsToChangePixel8Pro.put("MODEL", "Pixel 8 Pro");
-        propsToChangePixel8Pro.put("ID", "UQ1A.240205.004");
-        propsToChangePixel8Pro.put("FINGERPRINT", "google/husky/husky:14/UQ1A.240205.004/11269751:user/release-keys");
+        propsToChangePixel8Pro.put("ID", "AP1A.240505.005");
+        propsToChangePixel8Pro.put("FINGERPRINT", "google/husky/husky:14/AP1A.240505.005/11677807:user/release-keys");
         propsToChangePixel5a = new HashMap<>();
         propsToChangePixel5a.put("BRAND", "google");
         propsToChangePixel5a.put("MANUFACTURER", "Google");
@@ -149,8 +149,8 @@ public class DevicePropsSpoofing {
         propsToChangePixel5a.put("PRODUCT", "barbet");
         propsToChangePixel5a.put("HARDWARE", "barbet");
         propsToChangePixel5a.put("MODEL", "Pixel 5a");
-        propsToChangePixel5a.put("ID", "UQ1A.240205.002");
-        propsToChangePixel5a.put("FINGERPRINT", "google/barbet/barbet:14/UQ1A.240205.002/11224170:user/release-keys");
+        propsToChangePixel5a.put("ID", "AP1A.240505.004");
+        propsToChangePixel5a.put("FINGERPRINT", "google/barbet/barbet:14/AP1A.240505.004/11583682:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
